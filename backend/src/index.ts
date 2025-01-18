35375441767000593227f744c54de0beb3895372
konhito <konhito02@gmail.com>
2025-01-18 11:46:33 +0530

    deployed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 import { userRouter } from "./routes/User";
 import { blogRouter } from "./routes/Blog";
 import { cors } from "hono/cors";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const app = new Hono<{
   Bindings: {
@@ -12,5 +14,11 @@ const app = new Hono<{
 app.use("/*", cors());
 app.route("/user", userRouter);
 app.route("/blog", blogRouter);
+app.get("/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  return c.text("working");
+});
 
 export default app;
